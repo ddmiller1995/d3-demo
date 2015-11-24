58cf04a4c88b0dbbd9b94876ece97a1896d57267
iGuest <ddmiller1995@gmail.com>
2015-11-24 12:21:52 -0800

    Inclass work

--- js/main.js
+++ js/main.js
@@ -27,11 +27,19 @@ $(function() {
 			.attr('width', width)
 
 	// Write a function to set your scales
+	var xScale;
+	var yScale;
 	var setScales = function() {
 		// xScale
-		
+		var xMin = d3.min(data, function(d) {return d.le_1960});
+		var xMax = d3.max(data, function(d) {return d.le_1960});
+		xScale = d3.scale.linear().domain([xMin, xMax]).range([0, width]);
 
-		// yScale		
+		// yScale	
+		var yMin = d3.min(data, function(d) {return d.le_2013});
+		var yMax = d3.max(data, function(d) {return d.le_2013});
+		yScale = d3.scale.linear().domain([yMax, yMin]).range([0, height]);
+	
 	}
 
 	/* Write a function to define the positioning of your circles
@@ -40,7 +48,12 @@ $(function() {
 		- title attribute as the country of the object
 	*/
 	var circleFunc = function(circle) {
-		
+		circle.attr("cx", function(d) {return xScale(d.le_1960)})
+		.attr("cy", function(d) {return yScale(d.le_2013)})
+		.attr("title", function(d) {return d.country})
+		.attr("r", 10)
+		.attr("fill", "blue")
+		.style("opacity", .5);
 
 
 	}
@@ -48,29 +61,29 @@ $(function() {
 	// Write a reusable drawing function for circles
 	var draw = function(data) {
 		// Set Scales
-		
+		setScales();
 		
 		// Select all circles and bind your data to them
-		
+		var circles = g.selectAll("circle").data(data, function(d) {return d.country});
 	
 		// Use the .enter() method to get your entering elements, and then position them using your positioning function
-    	
+    	circles.enter().append("circle").call(circleFunc);
 	
   
 	    // Use the .exit() and .remove() methods to remove elements that are no longer in the data
-		
+		circles.exit().remove();
 	  
 	    // Select all circle elements within your g and transition their position using your positioning function
-		
+		g.selectAll("circle").transition().duration(500).call(circleFunc)
 
 	
 	}
 
 	// Pass data to your drawing function
-	
+	draw(data);
 
 	// Define x axis using d3.svg.axis(), assigning the scale as the xScale
-	
+
 
 
 	// Define y axis using d3.svg.axis(), assigning the scale as the yScale
